4587326ffde5c4354eb12bc877ae0d63398cdc07
AntoniaGrueber <Antonia.grueber@t-online.de>
2025-11-05 14:42:47 +0100

    Update H2

--- h2/H2_main.java
+++ h2/H2_main.java
@@ -5,9 +5,7 @@ public class H2_main {
 public static void main(String[] args) {
 	int Jahr = 2025;
 	boolean schalt = false;
-	if (Jahr % 4 ==0 && Jahr % 100 !=0) {
-		schalt = true;
-	} else if (Jahr % 100 ==0 && Jahr % 400==0) {
+	if (Jahr % 4 ==0 && Jahr % 100 !=0)||(Jahr % 400){
 		schalt = true;
 	}
 	
@@ -15,3 +13,4 @@ public static void main(String[] args) {
 }
 
 }
+
